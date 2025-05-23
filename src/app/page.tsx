eaa3608f476cdc80af2a4af04c4c55f1eadb0261
Korng Samnang <korngsamnang168@gmail.com>
2025-05-23 14:48:46 +0700

    init: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export const Page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">Hello Nextjs 15.3</h1>
+            <Button>Hello Button</Button>
+        </div>
+    );
+};
+
+export default Page;
